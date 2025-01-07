83bdfd172e483eda6f8b49662e4d36d72e1a311e
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2025-01-07 20:33:42 +0100

    Add gsap animations

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,6 +1,25 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
-
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      x: 500,
+      duration: 1.5,
+      rotation: 360,
+      borderRadius: "100%",
+      ease: "back.inOut",
+      // stagger: 0.5,
+      stagger: {
+        amount: 1.5,
+        grid: [2, 1],
+        axis: "y",
+        ease: "circ.inout",
+        from: "center",
+      },
+    });
+  }, []);
   return (
     <main>
       <h1>GsapStagger</h1>
